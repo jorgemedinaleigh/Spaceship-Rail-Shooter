05a1b067c60c5d201f3cd102d84bc088c6b1c8f0
Jorge Medina <jorgemedinaleigh@protonmail.com>
2021-08-26 13:01:39 -0400

    Added enemy explosion & and score management and UI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int score = 5;
+
+    ScoreBoard scoreBoard;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     // Update is called once per frame
@@ -18,6 +24,14 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
+        KillEnemy();
+        scoreBoard.IncreaseScore(score);        
+    }
+
+    private void KillEnemy()
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
