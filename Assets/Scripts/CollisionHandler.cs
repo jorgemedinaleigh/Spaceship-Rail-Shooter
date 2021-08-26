d014d5cd5288c419e4253780c411e59f5d2c2eb0
Jorge Medina <jorgemedinaleigh@protonmail.com>
2021-08-25 21:12:59 -0400

    Added functionality to the player crashing

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem crashVFX;
+
+    private GameObject ship;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ship = GameObject.Find("Example5_NoInteriorGrey");
     }
 
     // Update is called once per frame
@@ -16,13 +23,22 @@ public class CollisionHandler : MonoBehaviour
         
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnTriggerEnter(Collider other)
     {
-
+        StartCrashSequence();
     }
 
-    private void OnTriggerEnter()
+    private void StartCrashSequence()
     {
-        
+        crashVFX.Play();
+        GetComponent<PlayerController>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+        ship.SetActive(false);
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
