6eb00774192397461598aac372451c6beb210c96
Jorge Medina <jorgemedinaleigh@protonmail.com>
2021-08-30 00:10:30 -0400

    Added Hit Points to enemies and created 3 different enemy spaceships

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
-    [SerializeField] Transform parent;
+    
     [SerializeField] int score = 5;
+    [SerializeField] int hitPoints = 5;
 
     ScoreBoard scoreBoard;
+    GameObject parentGameObject;
 
     // Start is called before the first frame update
     void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        parentGameObject = GameObject.FindWithTag("Spawn at Runtime");
+        AddRigidbody();
+    }
+
+    private void AddRigidbody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     // Update is called once per frame
@@ -23,15 +33,25 @@ public class Enemy : MonoBehaviour
     }
 
     private void OnParticleCollision(GameObject other)
-    {
-        KillEnemy();
-        scoreBoard.IncreaseScore(score);        
+    {    
+        ProcessHit();  
+
+        if(hitPoints < 1)
+        {
+            KillEnemy();
+        }        
     }
 
     private void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         Destroy(gameObject);
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+        scoreBoard.IncreaseScore(score);  
     }
 }
